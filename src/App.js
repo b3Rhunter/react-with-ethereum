db81e26b58df9851847a9934dd551d96d58adbaf
Pub <96320073+b3Rhunter@users.noreply.github.com>
2023-02-27 11:42:26 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { ethers } from "ethers";
 function App() {
 
   const [connected, setConnected] = useState(false);
+  const [name, setName] = useState("please sign in");
 
   const connect = async () => {
     try {
@@ -13,6 +14,22 @@ function App() {
       await provider.send("eth_requestAccounts", []);
       const signer = provider.getSigner()
       await signer.signMessage("Welcome to React With Ethereum!");
+
+      const { ethereum } = window;
+      if(ethereum) {
+        const ensProvider = new ethers.providers.InfuraProvider('mainnet');
+        const signer = provider.getSigner();
+        const address = await signer.getAddress();
+        const displayAddress = address?.substr(0, 6) + "...";
+        const ens = await ensProvider.lookupAddress(address);
+        if (ens !== null) {
+          setName("welcome back..."+ens)
+        } else {
+          setName("welcome back..."+displayAddress)
+        }
+      } else {
+        alert('no wallet detected!')
+      }
       setConnected(true)
     } catch (error) {
       alert(error.message)
@@ -21,12 +38,14 @@ function App() {
 
   const disconnect = async () => {
     setConnected(false)
+    setName("please sign in")
   }
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>React With Ethereum</h1>
+        <h3>{name}</h3>
 
         {!connected && (
           <button onClick={connect}>connect</button>
